67cd8f160b5ee1246fd5a65712c4f6ccb43d3c9d
ngunnnn <ngunnnn@ngunnnns-MacBook-Pro.local>
2019-04-14 05:16:49 +0700

    sign in email

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, View} from 'react-native';
+import {Platform, Image, View} from 'react-native';
 import { Container, Button, Content, Form, Item, Input, Picker, Icon, Text } from 'native-base';
-import firebase from '../firebase'
+import firebase from 'react-native-firebase'
 
 // import HeaderBar from './HeaderBar'
 // import FooterBar from './FooterBar'
@@ -21,8 +21,15 @@ export default class SignIn extends Component {
 
   constructor(props) {
     super(props);
+    this.unsubscriber = null;
     this.state = {
-      selected2: undefined
+      selected2: undefined,
+      isAuthenticated: false,
+      typedEmail: '',
+      typedPassword: '',
+      user: null,
+      showStatus: '',
+      errorStatus: '',
     };
   }
   onValueChange2(value: string) {
@@ -30,10 +37,56 @@ export default class SignIn extends Component {
       selected2: value
     });
   }
+
+  componentDidMount() {
+    this.unsubscriber = firebase.auth().onAuthStateChanged((changedUser) => {
+      this.setState({ user: changedUser })
+    })
+  }
+  
+  componentWillUnmount(){
+    if( this.unsubscriber ){
+      this.unsubscriber()
+    }
+  }
+
+  onRegister = () => {
+    firebase.auth().createUserWithEmailAndPassword(this.state.typedEmail, this.state.typedPassword)
+    .then((signInUser) => {
+      this.setState({ user: signInUser })
+      console.log(`Register with user : ${JSON.stringify(signInUser.toJSON())}`)
+    }).catch((error) => {
+      console.log(`Register fail with error : ${error}`)
+      this.setState({
+        errorStatus: `${error}`
+      })
+      const errorword = this.state.errorStatus.split(':')
+      if(errorword[0] == 'Error'){
+        this.setState({
+          showStatus: errorword[1]
+        })
+      }
+    })
+  }
+
+  onSignIn = () => {
+    firebase.auth().signInWithEmailAndPassword(this.state.typedEmail, this.state.typedPassword)
+    .then((signInUser) => {
+      this.setState({user: signInUser})
+      console.log(`Register with user : ${JSON.stringify(signInUser.toJSON())}`)
+    }).catch((error) => {
+      console.log(`Register fail with error : ${error}`)
+    })
+  }
   render() {
     return (
     <Container style={{backgroundColor:'#ffca28'}}>
-        <Content style={{marginTop: '60%'}}>
+        <Content style={{marginTop: '40%', flex: 1}}>
+        <Image fontSize source={{uri: 'https://firebasestorage.googleapis.com/v0/b/project-190f0.appspot.com/o/Heartbeat-III.png?alt=media&token=a517cee4-b564-4713-bd18-d48970959183'}} 
+        style={{ alignSelf:'center' ,width: 150, height: 150, flex: 1}}/>
+        <Text style={{paddingTop: "1%", alignSelf: 'center', fontWeight:'700', fontSize:20}}>TRAVEL  IS  LIFE</Text>
+        <Text style={{paddingBottom: "5%", alignSelf: 'center'}}>Let's create the life you love.</Text>
+        
           <Form>
             <Item picker style={{borderColor: '#A9A9A9'}} >
               <Picker
@@ -52,17 +105,20 @@ export default class SignIn extends Component {
             </Item>
 
             <Item last style={{borderColor: '#A9A9A9'}}>
-              <Input placeholder="Username" />
+              <Input placeholder="E-mail" autoCorrect={false} autoCapitalize='none' onChangeText={(text) =>{this.setState({ typedEmail : text })}} />
             </Item>
 
             <Item last style={{borderColor: '#A9A9A9'}}>
-              <Input placeholder="Password" />
+              <Input placeholder="Password" autoCorrect={false} autoCapitalize='none' secureTextEntry={true} onChangeText={(text) =>{this.setState({ typedPassword : text ,showStatus: ''})}} />
             </Item>
             
-            <View style={{paddingTop: "20%", alignSelf: 'center', flexDirection:'row'}}>
-              <Button style={{marginRight:'5%'}} dark><Text> Sign Up </Text></Button>
-              <Button dark ><Text> Sign In </Text></Button>
+            <Text style={{color: 'red', paddingTop:"4%", textAlign:'left', fontSize:13}} >{this.state.showStatus}</Text>
+
+            <View style={{margin:'3%'}}>
+              <Button block style={{marginBottom: '2%', backgroundColor: '#E1AD01'}} onPress={this.onSignIn} ><Text> Sign In </Text></Button>
+              <Button block style={{backgroundColor: '#281e5d'}}onPress={this.onRegister} dark><Text> Sign Up </Text></Button>
             </View>
+            
             
 
           </Form>
